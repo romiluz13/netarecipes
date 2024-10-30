fedfc54d3683ab13e3b3be654df7782904ce2c43
romiluz13 <rom@iluz.net>
2024-10-30 21:36:38 +0200

    Initial commit

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,13 +3,14 @@ import { getFirestore, enableIndexedDbPersistence } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 import { getAuth } from 'firebase/auth';
 
+// יש להעביר את המפתחות הללו ל-.env
 const firebaseConfig = {
-  apiKey: "AIzaSyCE_P_ry3W9V2aHAFVWo3iK0Np8yEtFdZg",
-  authDomain: "neta-recipes.firebaseapp.com",
-  projectId: "neta-recipes",
-  storageBucket: "neta-recipes.appspot.com",
-  messagingSenderId: "802923641287",
-  appId: "1:802923641287:web:2cbc4d3211eca8188301e2"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // אתחול Firebase
